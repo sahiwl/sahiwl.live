9338c6610e4f91c24ee985ccdb0c99db75495a58
sahiwl <piyushkrray1@gmail.com>
2025-01-18 16:20:36 +0530

    fix: more dimensions 💀

--- app/links/page.tsx
+++ app/links/page.tsx
@@ -4,7 +4,7 @@ import LinksComp from "@/components/Links"
 export default function links() {
   return (
     <main className="dark:text-zinc-200 min-h-full w-full">
-      <section className="flex flex-col items-center justify-between md:py-10 py-24 mx-8 md:mx-10 md:my-20 my-20">
+      <section className="flex flex-col items-center justify-between md:py-10  mx-8 md:mx-10 md:my-20 my-20">
         <div className="" id="heading">
           <h3 className="font-exo2">Links</h3>
           <p>me on the internet in different aliases-</p>
