5892e5be55f5b81df3c3c9e9663642a36b3a7498
sahiwl <piyushkrray1@gmail.com>
2025-02-09 19:52:34 +0530

    chore: removed cmdbar and added loading

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@
     export const Footer = () => {
         return (
             // <div className="p-10 w-full max-h-[330px] bottom-0 dark:border-cyan-400 border-t-2 flex justify-around">
-            <footer className="flex px-4 py-4 justify-between w-full border-t-[1px] dark:border-cyan-400 backdrop-blur-xl shadow-xl border-[#4e201c]" >
+            <footer className="bottom-0 flex px-4 py-4 justify-between w-full border-t-[1px] dark:border-cyan-400 backdrop-blur-xl shadow-xl border-[#4e201c]" >
                 <span className="text-zinc-500 font-semibold font-exo2">
                 made by sahil 😉
                 </span>
